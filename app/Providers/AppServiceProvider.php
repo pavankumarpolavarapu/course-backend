c0613a9fad4d304fdc70c8418afb77742eaf49fb
Pavan <pavan.vnr@gmail.com>
2016-04-05 09:18:37 +0530

    Commit-002 - Added Laracasts/Generators to Dev Requirement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //Registering Laracasts/Generators for Development
+        if ($this->app->environment() == 'local') {
+            $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+        }
     }
 }
